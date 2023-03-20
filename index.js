b7bb65be5f01253cb00859948aadd782663886ce
Bryant Lin <bryantmlin@gmail.com>
2023-03-20 11:40:08 -0600

    control flow lab 1

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
   // Write your code here!
+  if (value<400)
+  {
+    return "This one is on me!"
+  }
+  else if (value>=400 && value < 2000)
+  {
+    return "That will be twenty bucks."
+  }
+  else if (value>=2000 && value<2500)
+  {
+    return "I will gladly take your thirty bucks."
+  }
+  else return "No can do."
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const check = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return check;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  let response;
+  switch (tip) {
+    case 'generous':
+      response = "Thank you so much.";
+      break;
+    case 'not as generous':
+      response = "Thank you.";
+      break;
+    default:
+      response = "Bye.";
+      break;
+  }
+  return response;
   // Write your code here!
+
 }
